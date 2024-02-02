28e85217fc77a390f56e78b6ebcfb819858edde4
ivanblinov2k17 <ivanblinov2015@mail.ru>
2024-02-02 11:06:42 +0400

    Update example for 23.2

--- app.vue
+++ app.vue
@@ -1,13 +1,16 @@
 <template>
-  <button-example />
+  <dx-button text="Sample button" @click="onClick"/>
 </template>
 
 <script>
-import ButtonExample from "./components/button-example";
+import { DxButton } from "./devextreme-bundle/devextreme-vue-bundle";
 
 export default {
+  methods: {
+    onClick(){ console.log("works")} 
+  },
   components: {
-    ButtonExample
-  }
+    DxButton,
+}
 };
 </script>
